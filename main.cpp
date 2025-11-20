10a04d554114165aee4b96fe274c5fa42974e7e0
vanna066735375 <vannaa123c@gmail.com>
2025-11-20 15:24:39 +0700

    delete

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "ui/homepage.hpp"
+ 
+using namespace std;
+int main(){
+
+    system("cls");
+    typeWrite("                         Initializing system...\n");
+    homePage();
+    
+    return 0;
+}
